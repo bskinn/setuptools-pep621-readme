127ba9018ad9802790236547d7e680016263cecb
Brian Skinn <brian.skinn@gmail.com>
2022-04-03 17:27:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# setuptools-pep621-readme: Test repo to probe PEP621 readme/long-description behavior
+
+Stub repository for a MVR of readme/long_description misbehavior under PEP621.
+
+----
+
+Source on [GitHub](https://github.com/bskinn/jupyter-tempvars).
+
+Copyright (c) Brian Skinn 2022
+
+License: The MIT License. See
+[LICENSE.txt](https://github.com/bskinn/sphobjinv/blob/main/LICENSE.txt)
+for full license terms.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+NAME = "setuptools-pep621-readme"
+
+# Would actually retrieve this dynamically from package version.py
+__version__ = "0.1.dev1"
+
+# Would actually carry out a dynamic substition of selected content
+# from the project README.md
+readme = "This is a test readme."
+
+setup(
+    version=__version__,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    # readme=readme(),
+)
